992d340fc7d32669c84635a893046c96d75bfbd0
coded <urmcoded@outlook.com>
2026-08-20 10:56:11 +0100

    build.bat stuff

--- build.bat
+++ build.bat
@@ -1,2 +1,8 @@
-cmake -H. -Bbuild ^  -DCMAKE_TOOLCHAIN_FILE="C:\Users\coded\Downloads\android-ndk-r27d-windows\android-ndk-r27d\build\cmake\android.toolchain.cmake" ^  -DCMAKE_MAKE_PROGRAM="C:\Users\coded\Downloads\android-ndk-r27d-windows\android-ndk-r27d\prebuilt\windows-x86_64\bin\make.exe" ^  -DANDROID_ABI=arm64-v8a ^  -DANDROID_PLATFORM=android-24 ^  -G "MinGW Makefiles"
-cmake --build build
+@echo off
+REM Edit these two variables to match your environment - should look like this
+set NDK_DIR=C:\Users\coded\Downloads\android-ndk-r27d-windows\android-ndk-r27d
+set CMAKE_EXE=C:\Program Files\CMake\bin\cmake.exe
+
+REM Configure and build for Android (adjust ANDROID_ABI/ANDROID_PLATFORM as needed)
+"%CMAKE_EXE%" -S . -B build -DCMAKE_TOOLCHAIN_FILE="%NDK_DIR%\build\cmake\android.toolchain.cmake" -DCMAKE_MAKE_PROGRAM="%NDK_DIR%\prebuilt\windows-x86_64\bin\make.exe" -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-24 -G "MinGW Makefiles"
+"%CMAKE_EXE%" --build build
